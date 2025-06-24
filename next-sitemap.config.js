6fe38fecd6650a5b12939ef12a467c781f6c5714
sahryahmd <sahry10@gmail.com>
2025-06-24 14:01:30 +0700

    update

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteUrl: "https://www.spincityagora.my.id",
-  generateRobotsTxt: true, // (optional)
-  // optional
-  robotsTxtOptions: {
-    policies: [{ userAgent: "*", allow: "/" }],
-  },
-}
